120fbecf1620f023f4dab27d06f9ef89d27b7414
piyush <sainipiyus11111@gmail.com>
2023-01-02 17:42:08 +0530

    new code2

--- magic2.js
+++ magic2.js
@@ -1,11 +1,5 @@
-var header =document.getElementById("main-header")
-header.style.borderBottom="solid 3px #000"
-var items= document.getElementsByClassName("list-group-item")
-console.log(items[1])
-items[2].style.background = "yellow"
-items[0].style.fontWeight='bold'
-items[1].style.fontWeight='bold'
-items[2].style.fontWeight='bold'
-items[3].style.fontWeight='bold'
-document.getElementById("1").style.color = "green";
-
+var li= document.getElementsByClassName('new-text')
+console.log(li)
+li[0].style.fontWeight='bold'
+var li= document.getElementsByTagName('li')
+li[4].style.background='green'
