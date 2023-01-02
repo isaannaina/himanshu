9db6ce2fdcfaf8762de8f187bb2d5ee8ef3c1a77
piyush <sainipiyus11111@gmail.com>
2023-01-02 16:20:23 +0530

    list code

--- magic2.js
+++ magic2.js
@@ -1,6 +1,11 @@
 var header =document.getElementById("main-header")
 header.style.borderBottom="solid 3px #000"
-//var items= document.getElementsByClassName("list-group-item")
-//onsole.log(items[1])
-//tems[2].style.background = "yellow"
+var items= document.getElementsByClassName("list-group-item")
+console.log(items[1])
+items[2].style.background = "yellow"
+items[0].style.fontWeight='bold'
+items[1].style.fontWeight='bold'
+items[2].style.fontWeight='bold'
+items[3].style.fontWeight='bold'
 document.getElementById("1").style.color = "green";
+
