93ca778f68db6dde6ea04f4a646b4b1ced724ab9
piyush <sainipiyus11111@gmail.com>
2023-01-07 14:07:28 +0530

    add username on screen

--- magic2.js
+++ magic2.js
@@ -1,6 +1,18 @@
-var li= document.getElementsByClassName('new-text')
-console.log(li)
-li[0].style.fontWeight='bold'
-var li= document.getElementsByTagName('li')
-li[4].style.background='green'
+var form = document.getElementById('calltime')
 
+// Form submit event
+form.addEventListener('submit', addItem);
+function addItem(e){
+    e.preventDefault();
+  
+    // Get input value
+    var newItem = document.getElementById('calltime').value;
+  
+    // Create new li element
+    var li = document.createElement('li');
+    // Add class
+    li.className = 'form-control';
+    // Add text node with input value
+    ///li.appendChild(document.createTextNode(newItem))      background-image: url('https://media.istockphoto.com/id/1432854572/photo/young-superman-looking-and-contemplating-towards-salt-lake.jpg?s=612x612&w=is&k=20&c=PrMoWnKNDbToXrTyjdY986naFQCpdsxCpmOZ874Bd18=')
+
+}    //  background-image: url('https://media.istockphoto.com/id/1432854572/photo/young-superman-looking-and-contemplating-towards-salt-lake.jpg?s=612x612&w=is&k=20&c=PrMoWnKNDbToXrTyjdY986naFQCpdsxCpmOZ874Bd18=')
