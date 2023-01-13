a0e743ee1471061e0c0812525d8f1f53053cc2ff
piyush <sainipiyus11111@gmail.com>
2023-01-13 16:52:30 +0530

    title bold

---
+++ arch.js
@@ -0,0 +1,9 @@
+var headerTitle= document.getElementById('main-header')
+//headerTitle.style.borderBottom='solid 3px #000'
+headerTitle.style.borderBottom='solid 3px #000'
+//header.Title.textContent="the
+var eaderTitle= document.getElementById('main')
+eaderTitle.style.color='green'
+eaderTitle.style.fontWeight='bold'
+//title.style.color='red'
+console.log('main header')
--- magic2.js
+++ magic2.js
@@ -1,18 +1,1 @@
-var form = document.getElementById('calltime')
-
-// Form submit event
-form.addEventListener('submit', addItem);
-function addItem(e){
-    e.preventDefault();
-  
-    // Get input value
-    var newItem = document.getElementById('calltime').value;
-  
-    // Create new li element
-    var li = document.createElement('li');
-    // Add class
-    li.className = 'form-control';
-    // Add text node with input value
-    ///li.appendChild(document.createTextNode(newItem))      background-image: url('https://media.istockphoto.com/id/1432854572/photo/young-superman-looking-and-contemplating-towards-salt-lake.jpg?s=612x612&w=is&k=20&c=PrMoWnKNDbToXrTyjdY986naFQCpdsxCpmOZ874Bd18=')
-
-}    //  background-image: url('https://media.istockphoto.com/id/1432854572/photo/young-superman-looking-and-contemplating-towards-salt-lake.jpg?s=612x612&w=is&k=20&c=PrMoWnKNDbToXrTyjdY986naFQCpdsxCpmOZ874Bd18=')
+console.log("thes")
