7999b7d2c6c1c05fc67bea31d7f54b2271d2ec8b
piyush <sainipiyus11111@gmail.com>
2023-01-16 00:18:47 +0530

    this keyword

--- arch.js
+++ arch.js
@@ -1,33 +1,32 @@
-//answer no.1)
-let obj={nums:7}
-function add(n){
-    console.log(this.nums+n)
-}
-add.call(obj,8)
-
-//answer No.2)==>
-
-let obj1={nums:7}
-function add(n){
-    console.log(this.nums+n)
-}
-add.apply(obj1,[8])
-//answer3)
-
-let obj3={nums:7}
-function add(n){
-    console.log(this.nums+n)
-
-}
-let abc=(add.bind(obj3,7))
-console.log(abc)
-abc()
-
-//answer No.4))=>
-
-let student={age:20}
-function age(){
-    console.log(this.age)
-}
-var add= age.bind(student)
-add()
+//console.log("Hello, World!");
+class student{
+    constructor(name, age , marks)
+    {
+      this.name=name
+      this.age=age
+      this.marks=marks
+    }
+    
+    function(minplacement){
+      console.log(this)
+      return (minmarks)=>{
+    
+    console.log('inside elegibleforcurrentcompany',this)
+    if(this.marks>minmarks &&this.age >minplacement){
+      console.log(this.log+"ready for placement")
+    }else
+    {
+      console.log(this.name +"he is nor ready for the placement")
+    }
+    }
+    
+    }
+    }
+    var obj1=new student("jack",'28','777')
+    var obj2=new student("jac",'27',664)
+    obj1.function(19)(79)
+    
+    var obj3=new student("jacki",'20')
+    var obj5=new student("jackt",'22')
+    
+    obj1.function(55)
