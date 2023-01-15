940a89c19bd83d0094a6733c23d5009dd7ba58d6
piyush <sainipiyus11111@gmail.com>
2023-01-15 12:27:01 +0530

    call apply and bind

--- arch.js
+++ arch.js
@@ -1,11 +1,33 @@
-var title= document.querySelectorAll('.list-group-item')
-title[1].style.backgroundColor='green'
-title[2].textContent=''
+//answer no.1)
+let obj={nums:7}
+function add(n){
+    console.log(this.nums+n)
+}
+add.call(obj,8)
 
-var title= document.querySelector('input')
-//title.value='hell'
-//var odd= document.querySelectorAll('li:nth-child(odd)')
-///for(i=0;i<odd.length;i++)
-//{
-    //odd[i].style.backgroundColor='#CCC'
-//}
+//answer No.2)==>
+
+let obj1={nums:7}
+function add(n){
+    console.log(this.nums+n)
+}
+add.apply(obj1,[8])
+//answer3)
+
+let obj3={nums:7}
+function add(n){
+    console.log(this.nums+n)
+
+}
+let abc=(add.bind(obj3,7))
+console.log(abc)
+abc()
+
+//answer No.4))=>
+
+let student={age:20}
+function age(){
+    console.log(this.age)
+}
+var add= age.bind(student)
+add()
